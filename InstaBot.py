45722a11925a389736bd34f6976f4aa479019f39
McClane Howland <mcclane.howland@gmail.com>
2017-06-22 13:18:40 -0700

    updated import name

--- InstaBot.py
+++ InstaBot.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 import time
-import InstagramLibrary as insta
+import InstagramBotLibrary as insta
 
 #set up mobile emulation
 mobile_emulation = {"deviceName": "Google Nexus 5"}
