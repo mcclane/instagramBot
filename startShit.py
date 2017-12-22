186926bae8f4ca2c8a10b3e4f903ebd05efbf3d6
McClane Howland <mcclane.howland@gmail.com>
2017-12-22 21:20:11 +0000

    remove start script because it didn't work

--- startShit.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-import sys
-
-with open("users.txt", "r") as f:
-    for line in f.readlines():
-        username, password = line.split(",")
-        subprocess.Popen(["nohup", "python3", "controller.py", "--username", username, "--password ", password])
-        print("Started "+username)
